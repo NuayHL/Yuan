3f209faa02572ddf1ab8af762b4ab7666446090e
NuayHL <liuhy.df.xin@gmail.com>
2022-11-19 14:42:43 +0900

    Initial commit

---
+++ yuan.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+class Yuan:
+    def __init__(self):
+        self._BASE_PATH = os.getcwd()
+        self._PATH = None
+
+    def set_path(self, path=''):
+        pass
+
+    def get_path(self):
+        return self._PATH
+    
