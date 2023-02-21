3a7539c73a84829024253b094707eb73e3c827a0
NuayHL <liuhy.df.xin@gmail.com>
2023-02-22 03:28:36 +0900

    advanced probar and colorstr

---
+++ colorstr.py
@@ -0,0 +1,114 @@
+class ColorStr:
+    @staticmethod
+    def thick(strings):
+        return f'\033[1m{strings}\033[0m'
+
+    @staticmethod
+    def underline(strings):
+        return f'\033[4m{strings}\033[0m'
+
+    @staticmethod
+    def yellow(strings):
+        return f'\033[33m{strings}\033[0m'
+
+    @staticmethod
+    def thick_yellow(strings):
+        return f'\033[1;33m{strings}\033[0m'
+
+    @staticmethod
+    def light_yellow(strings):
+        return f'\033[93m{strings}\033[0m'
+
+    @staticmethod
+    def thick_light_yellow(strings):
+        return f'\033[1;93m{strings}\033[0m'
+
+    @staticmethod
+    def green(strings):
+        return f'\033[32m{strings}\033[0m'
+
+    @staticmethod
+    def light_green(strings):
+        return f'\033[92m{strings}\033[0m'
+
+    @staticmethod
+    def thick_green(strings):
+        return f'\033[1;32m{strings}\033[0m'
+
+    @staticmethod
+    def thick_light_green(strings):
+        return f'\033[1;92m{strings}\033[0m'
+
+    @staticmethod
+    def red(strings):
+        return f'\033[31m{strings}\033[0m'
+
+    @staticmethod
+    def thick_red(strings):
+        return f'\033[1;31m{strings}\033[0m'
+
+    @staticmethod
+    def light_red(strings):
+        return f'\033[91m{strings}\033[0m'
+
+    @staticmethod
+    def thick_light_red(strings):
+        return f'\033[1;91m{strings}\033[0m'
+
+    @staticmethod
+    def blue(strings):
+        return f'\033[34m{strings}\033[0m'
+
+    @staticmethod
+    def thick_blue(strings):
+        return f'\033[1;34m{strings}\033[0m'
+
+    @staticmethod
+    def light_blue(strings):
+        return f'\033[94m{strings}\033[0m'
+
+    @staticmethod
+    def thick_light_blue(strings):
+        return f'\033[1;94m{strings}\033[0m'
+
+    @staticmethod
+    def purple(strings):
+        return f'\033[35m{strings}\033[0m'
+
+    @staticmethod
+    def thick_purple(strings):
+        return f'\033[1;35m{strings}\033[0m'
+
+    @staticmethod
+    def light_purple(strings):
+        return f'\033[95m{strings}\033[0m'
+
+    @staticmethod
+    def thick_light_purple(strings):
+        return f'\033[1;95m{strings}\033[0m'
+
+    @staticmethod
+    def cyan(strings):
+        return f'\033[36m{strings}\033[0m'
+
+    @staticmethod
+    def thick_cyan(strings):
+        return f'\033[1;36m{strings}\033[0m'
+
+    @staticmethod
+    def light_cyan(strings):
+        return f'\033[96m{strings}\033[0m'
+
+    @staticmethod
+    def thick_light_cyan(strings):
+        return f'\033[1;96m{strings}\033[0m'
+
+
+# testing
+if __name__ == '__main__':
+    a = 'haha [] == <> {} . , / \\ \' \"'
+    for func in ColorStr.__dict__:
+        if not func.startswith('__'):
+            print(func, end=': ')
+            print(getattr(ColorStr, func)(a))
+
--- progressbar.py
+++ progressbar.py
@@ -1,18 +1,24 @@
 from collections import Iterable
 from timer import Timer
+from colorstr import ColorStr as CO
+
 
 class IterProgressBar:
     def __init__(self, iters, lenth=None,
-                 barlenth=20, endstr='', instant_per_format='.1f', eta_on=False):
+                 barlenth=20, endstr='', percentage_formate='.1f', eta_on=False,
+                 colored=True, bar_color=None, mid_color=None, end_color=None):
         self._count = -1
         self._max_iter = 0
-        self._bar_len = barlenth
         self._end = endstr
         self._stop = False
         self._iter = iters
         self._temp_end_str = ''
-        self._instant_per_format = instant_per_format
         self._count_eta = eta_on
+        bar_formate = CO.light_yellow if bar_color is None else bar_color
+        eta_formate = CO.light_purple if mid_color is None else mid_color
+        end_formate = CO.light_green if end_color is None else end_color
+        self._GenBar = FormatBarPrint(barlenth=barlenth, percentage_formate=percentage_formate, colored=colored,
+                                      bar_formate=bar_formate, eta_formate=eta_formate, end_formate=end_formate)
 
         if self._count_eta:
             self._timer = Timer()
@@ -55,7 +61,7 @@ class IterProgressBar:
         else:
             raise NotImplementedError
 
-    def write(self, strs):
+    def write(self, strs: str):
         self._temp_end_str = strs
 
     def stat(self):
@@ -63,50 +69,49 @@ class IterProgressBar:
             return self._timer
 
     def _default_update(self):
-        self._update(endstr=self._temp_end_str)
+        self._update(write=self._temp_end_str)
         self._temp_end_str = ''
 
-    def _update(self, endstr=''):
+    def _update(self, write=''):
         self._count += 1
         if self._count >= self._max_iter:
-            endstr += '\n'
+            write += '\n'
             self._stop = True
-        percentage = float(self._count) / self._max_iter
-
-        if self._instant_per_format == 'num':
-            instant_per = '%d/%d' % (self._count, self._max_iter)
-        else:
-            formated_percentage = format(percentage * 100, self._instant_per_format)
-            instant_per = '%s' % formated_percentage + '%'
 
         if self._count_eta:
             self._timer.tick()
             if self._stop:
-                counts_eta = 'Total Time: ' + Timer.period_convert(self._timer.total_time())
+                counts_eta = f'[Total Time: {Timer.period_convert(self._timer.total_time())}]'
             else:
                 time_in_iter = self._timer.average_time()
                 counts_eta = (self._max_iter - self._count) * time_in_iter
-                counts_eta = 'ETA:' + Timer.period_convert(counts_eta)
+                counts_eta = '[ETA: ' + Timer.period_convert(counts_eta) + ']'
         else:
             counts_eta = ''
 
-        print('\r[' + '>' * int(percentage * self._bar_len) +
-              '-' * (self._bar_len - int(percentage * self._bar_len)) + ']',
-              instant_per, counts_eta,
-              end=' ' + self._end + ' ' + endstr)
+        endstr = write if self._end == '' else self._end + ' ' + write
+
+        self._GenBar.print(self._count, self._max_iter, counts_eta, endstr)
+
 
 class ManualProgressBar:
-    def __init__(self, max_iter, barlenth=20, endstr='', instant_per_format='.1f', eta_on=False):
+    def __init__(self, max_iter, barlenth=20, endstr='',
+                 percentage_formate='.1f', eta_on=False,
+                 colored=True, bar_color=None, mid_color=None, end_color=None):
         self._count = 0
         self._max_iter = 0
-        self._bar_len = barlenth
         self._end = endstr
         self._stop = False
         assert isinstance(max_iter, int) and max_iter > 0
         self._max_iter = max_iter
-        self._instant_per_format = instant_per_format
-
         self._count_eta = eta_on
+
+        bar_formate = CO.light_yellow if bar_color is None else bar_color
+        eta_formate = CO.light_purple if mid_color is None else mid_color
+        end_formate = CO.light_green if end_color is None else end_color
+        self._GenBar = FormatBarPrint(barlenth=barlenth, percentage_formate=percentage_formate, colored=colored,
+                                      bar_formate=bar_formate, eta_formate=eta_formate, end_formate=end_formate)
+
         if self._count_eta:
             self._timer = Timer()
 
@@ -129,34 +134,55 @@ class ManualProgressBar:
         if self._count >= self._max_iter:
             write += '\n'
             self._stop = True
-        percentage = float(self._count) / self._max_iter
-
-        if self._instant_per_format == 'num':
-            instant_per = '%d/%d' % (self._count, self._max_iter)
-        else:
-            formated_percentage = format(percentage * 100, self._instant_per_format)
-            instant_per = '%s' % formated_percentage + '%'
 
         if period_time:
             if self._stop:
-                counts_eta = 'Total Time: ' + Timer.period_convert(self._timer.total_time())
+                counts_eta = f'[Total Time: {Timer.period_convert(self._timer.total_time())}]'
             else:
                 counts_eta = (self._max_iter - self._count) * period_time
-                counts_eta = 'ETA:' + Timer.period_convert(counts_eta)
+                counts_eta = '[ETA: ' + Timer.period_convert(counts_eta) + ']'
         else:
             counts_eta = ''
 
-        print('\r[' + '>' * int(percentage * self._bar_len) +
-              '-' * (self._bar_len - int(percentage * self._bar_len)) + ']',
-              instant_per, counts_eta,
-              end=' ' + self._end + ' ' + write)
+        endstr = write if self._end == '' else self._end + ' ' + write
+
+        self._GenBar.print(self._count, self._max_iter, counts_eta, endstr)
 
 
-def light_progressbar(percentage, endstr='', barlenth=20):
-    if int(percentage)==1: endstr +='\n'
-    print('\r[' + '>' * int(percentage * barlenth) +
-          '-' * (barlenth - int(percentage * barlenth)) + ']',
+def light_progressbar(percentage, endstr: str = '', barlenth=20):
+    if int(percentage) == 1: endstr += '\n'
+    ilenth = int(percentage * barlenth)
+    print('\r[' + '>' * ilenth + '-' * (barlenth - ilenth) + ']',
           format(percentage * 100, '.1f') + '%', end=' '+endstr)
+
+
+class FormatBarPrint:
+    def __init__(self, barlenth=20, finish='>', remain='-', percentage_formate='.1f', colored=True,
+                 bar_formate=CO.light_yellow, eta_formate=CO.light_purple, end_formate=CO.light_green):
+        self.i = finish
+        self.o = remain
+        self.bl = barlenth
+        self.pf = percentage_formate
+        self.colored = colored
+        self.bar_formate = bar_formate
+        self.eta_formate = eta_formate
+        self.end_formate = end_formate
+
+    def print(self, current: int, total: int, midstr='', endstr=''):
+        percentage = float(current) / total
+        if self.pf == 'num':
+            percentage_str = '%d/%d' % (current, total)
+        else:
+            percentage_str = format(percentage * 100, self.pf) + '%'
+        ilenth = int(percentage * self.bl)
+        barstr = '[%s%s] %s' % (ilenth * self.i, (self.bl-ilenth) * self.o, percentage_str)
+
+        if self.colored:
+            barstr = self.bar_formate(barstr)
+            midstr = self.eta_formate(midstr)
+            endstr = self.end_formate(endstr)
+
+        print('\r%s %s ' % (barstr, midstr), end=endstr)
 
 
 
