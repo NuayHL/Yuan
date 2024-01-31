6e75e5d04e7302ceb7259201c65f61ba2f93d21d
NauyHL <liuhy.df.xin@gmail.com>
2024-01-31 11:42:35 +0900

    modified setup packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='yuan_dl',
     version='0.1.0',
-    packages=['Yuan'],
+    packages=find_packages(exclude=["test"]),
     install_requires=[
         'pyyaml>=6.0.1'
     ],
